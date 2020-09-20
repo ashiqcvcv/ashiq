3376efb577a00391e37ffe07713e529da466efc1
ashiqcvcv <ashiqc23@gmail.com>
2020-09-20 16:44:18 +0530

    Updates

---
+++ precache-manifest.c0cacc5d9d89dcae006e5f20226e3b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824dfd5a968b4c47d4def76514b6514e",
+    "url": "/ashiq/index.html"
+  },
+  {
+    "revision": "aef68a9152d81ce34bc4",
+    "url": "/ashiq/static/css/main.e4103c9a.chunk.css"
+  },
+  {
+    "revision": "95c0a75fbbf20fd622fb",
+    "url": "/ashiq/static/js/2.c252989a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ashiq/static/js/2.c252989a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef68a9152d81ce34bc4",
+    "url": "/ashiq/static/js/main.826f1ad0.chunk.js"
+  },
+  {
+    "revision": "a9c8639a5a220b76961d",
+    "url": "/ashiq/static/js/runtime-main.cfee30ad.js"
+  }
+]);
